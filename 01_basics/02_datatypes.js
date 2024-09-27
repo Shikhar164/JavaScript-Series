91400b9830d4695d180caffaaf9fcf37b380008d
SHIKHAR BHARDWAJ <109871639+Shikhar164@users.noreply.github.com>
2024-09-27 04:24:01 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+//! Types of datatypes
+
+//A. Primitive Datatypes
+//? 1.String : "Shikhar",'Shikhar'
+//? 2.Number : 3,3.2
+//? 3.BigInt : range bigger than numbers
+//? 4.Boolean: true or false,0 or 1
+//? 5.Undefined:value that is not defined yet
+//? 6.Null   : null or empty
+//? 7 Symbol : make values unique
+
+//B. Reference Datatypes (Non-primitive datatypes)
+//? 1.Object
+//? 2.Array
+//? 3.Functions
+
+
+//* Question 1
+console.log(typeof null)// object
+
+//* Question 2
+console.log(typeof NaN)//number
+
+//* Question 3
+console.log(typeof undefined)//undefined
